21c6def1598810113482fd76e5e65cabbad40f78
nab138 <56705177+nab138@users.noreply.github.com>
2024-09-29 20:38:25 -0400

    Try to fix testflight encryption with Info.ios.plist

--- src/tabs/Welcome.tsx
+++ src/tabs/Welcome.tsx
@@ -4,7 +4,11 @@ import { IDockviewPanelProps } from "dockview";
 const Welcome: React.FC<IDockviewPanelProps<{ title: string }>> = () => {
   return (
     <div style={{ padding: "20px" }}>
-      <h1>Welcome to ShrinkWrap! hi</h1>
+      <h1>Welcome to ShrinkWrap!</h1>
+      <p>
+        This build should've been published automatically through github
+        actions, no confirmation required
+      </p>
     </div>
   );
 };
