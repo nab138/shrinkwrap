0edaa2a91feb5e4991767c6947d2bf10c51407a4
nab138 <nab@nabdev.me>
2024-09-30 12:15:22 -0400

    Sim connection on mobile

--- src/tabs/Settings.tsx
+++ src/tabs/Settings.tsx
@@ -1,12 +1,47 @@
 import { IDockviewPanelProps } from "dockview";
 import { usePrefs } from "../utils/PrefsContext";
 import { Theme } from "@tauri-apps/api/window";
+import { platform } from "@tauri-apps/plugin-os";
+import { useEffect, useState } from "react";
+import { connect, createClient } from "../networktables/NetworkTables";
 
 const Settings: React.FC<IDockviewPanelProps<{ title: string }>> = () => {
   const { theme, setTheme, connectionIP, setConnectionIP } = usePrefs();
 
+  const [mobile, setMobile] = useState(false);
+  useEffect(() => {
+    let getPlatform = async () => {
+      const currentPlatform = await platform();
+      if (currentPlatform === "ios" || currentPlatform === "android") {
+        setMobile(true);
+      }
+    };
+    getPlatform();
+  }, []);
+
   return (
     <div className="pageContainer">
+      {mobile && (
+        <div>
+          <h1>Connection</h1>
+          <button
+            onClick={() => {
+              createClient(connectionIP);
+              connect();
+            }}
+          >
+            Connect
+          </button>
+          <button
+            onClick={() => {
+              createClient("127.0.0.1");
+              connect();
+            }}
+          >
+            Connect (sim)
+          </button>
+        </div>
+      )}
       <h1>Settings</h1>
       <ul
         style={{
