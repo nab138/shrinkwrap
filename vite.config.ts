9b5287f557bfbdd05df5449b7e98a547b4dcd7f7
nab138 <nab@nabdev.me>
2024-10-03 23:40:01 -0400

    Get rid of million lint from config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default (defineConfig(async () => ({
-  plugins: [react(), MillionLint.vite()],
+  plugins: [react()/*,MillionLint.vite()*/],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
