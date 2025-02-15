40a42dd168025539ae67ef66bb10f8ef82fc7522
nab138 <nab@nabdev.me>
2025-02-15 18:35:19 -0500

    add important buttons to board

--- src/tabs/ReefscapeButtonBoard.tsx
+++ src/tabs/ReefscapeButtonBoard.tsx
@@ -2,6 +2,18 @@ import { IDockviewPanelProps } from "dockview";
 import useNTState from "../ntcore-react/useNTState";
 import "./ReefscapeButtonBoard.css";
 
+const reefs: {
+  num: number;
+  name: string;
+}[] = [];
+for (let i = 0; i < 11; i++) {
+  let letter = String.fromCharCode(65 + i).toUpperCase();
+  reefs.push({
+    num: i,
+    name: `Reef ${letter}`,
+  });
+}
+
 const ReefscapeButtonBoard: React.FC<
   IDockviewPanelProps<{ id: string }>
 > = () => {
@@ -10,38 +22,36 @@ const ReefscapeButtonBoard: React.FC<
     "double[]",
     []
   );
+
   return (
     <div className="pageContainer">
       <div className="buttonBoard">
-        <button
-          onClick={() => {
-            setPressed([0, 1]);
-          }}
-        >
-          Reef A
-        </button>
-        <button
-          onClick={() => {
-            setPressed([0, 2]);
-          }}
-        >
-          Reef B
-        </button>
-        <button
-          onClick={() => {
-            setPressed([0, 3]);
-          }}
-        >
-          Reef C
-        </button>
-        <br />
-        <button
-          onClick={() => {
-            setPressed([2, 1]);
-          }}
-        >
-          L1
-        </button>
+        <div className="buttonPanel">
+          {reefs.map((reef) => (
+            <button key={reef.num} onClick={() => setPressed([0, reef.num])}>
+              {reef.name}
+            </button>
+          ))}
+        </div>
+        <div className="buttonPanel">
+          <button onClick={() => setPressed([2, 10])}>L1</button>
+          <button onClick={() => setPressed([2, 9])}>L2</button>
+          <button onClick={() => setPressed([2, 8])}>L3</button>
+          <button onClick={() => setPressed([2, 7])}>L4</button>
+        </div>
+        <div className="buttonPanel">
+          <button onClick={() => setPressed([1, 1])}>Intake L1</button>
+          <button onClick={() => setPressed([1, 2])}>Intake L2</button>
+          <button onClick={() => setPressed([1, 3])}>Intake L3</button>
+          <button onClick={() => setPressed([1, 5])}>Intake R3</button>
+          <button onClick={() => setPressed([1, 6])}>Intake R2</button>
+          <button onClick={() => setPressed([1, 7])}>Intake R1</button>
+        </div>
+        <div className="buttonPanel">
+          <button onClick={() => setPressed([1, 4])}>Algae Toggle</button>
+          <button onClick={() => setPressed([0, 11])}>Net</button>
+          <button onClick={() => setPressed([2, 12])}>Processor</button>
+        </div>
       </div>
     </div>
   );
